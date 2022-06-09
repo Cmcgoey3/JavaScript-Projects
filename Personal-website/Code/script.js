5552e4fc093f614f5b04c2848e2999c429df37be
Connor McGoey <98868502+Cmcgoey3@users.noreply.github.com>
2022-06-09 17:43:21 -0400

    Add files via upload

---
+++ Personal-website/Code/script.js
@@ -0,0 +1,43 @@
+// Project Cards
+const pythonCard = document.getElementById('python-projects');
+const javaCard = document.getElementById('java-projects');
+const cCard = document.getElementById('c-projects');
+const armCard = document.getElementById('arm-projects');
+const prologCard = document.getElementById('prolog-projects');
+const schemeCard = document.getElementById('scheme-projects');
+const jsCard = document.getElementById('JS-projects');
+
+const rickRollCard = document.getElementById('rick-roll');
+
+rickRollCard.addEventListener('click', function () {
+  window.location.href =
+    'https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley';
+});
+
+pythonCard.addEventListener('click', function () {
+  window.location.href = 'https://github.com/Cmcgoey3/Python-Projects';
+});
+
+javaCard.addEventListener('click', function () {
+  window.location.href = 'https://github.com/Cmcgoey3/Java-Projects';
+});
+
+cCard.addEventListener('click', function () {
+  window.location.href = 'https://github.com/Cmcgoey3/C-Projects';
+});
+
+armCard.addEventListener('click', function () {
+  window.location.href = 'https://github.com/Cmcgoey3/ARM-Projects';
+});
+
+prologCard.addEventListener('click', function () {
+  window.location.href = 'https://github.com/Cmcgoey3/Prolog-Projects';
+});
+
+schemeCard.addEventListener('click', function () {
+  window.location.href = 'https://github.com/Cmcgoey3/Scheme-Projects';
+});
+
+jsCard.addEventListener('click', function () {
+  window.location.href = 'https://github.com/Cmcgoey3/JavaScript-Projects';
+});
